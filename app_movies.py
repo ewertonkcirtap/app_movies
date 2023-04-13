b76abc8e3f325a5e7776b5b88c6088b9b10c2e48
ewertonkcirtap <91208331+ewertonkcirtap@users.noreply.github.com>
2023-04-13 10:38:30 -0300

    Update app_movies.py

--- app_movies.py
+++ app_movies.py
@@ -28,7 +28,7 @@ st.set_page_config(
 st.sidebar.text("""@ewertonkcirtap""")
 st.sidebar.write('\n')
 st.sidebar.write('\n')
-st.sidebar.write("""## Que tal aplicar alguns filtros para sugerir filmes à você :smile:""")
+st.sidebar.write("""## Que tal aplicar alguns filtros para sugerir filmes à você.""")
 st.sidebar.write('\n')
 
 ### Definindo Filtros - Selecao Multipla

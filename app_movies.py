0382f697a8636c5ccba3b988f45c8e1a204e1df2
ewertonkcirtap <91208331+ewertonkcirtap@users.noreply.github.com>
2023-04-13 10:15:09 -0300

    Update app_movies.py

--- app_movies.py
+++ app_movies.py
@@ -77,7 +77,7 @@ def rating(x,url="https://imdb8.p.rapidapi.com/title/get-ratings"):
 if filtro1 and filtro2 and filme =="":
     csv = convert_df(df1)
     st.write('')
-    st.write(f"## *Dá um olhada nos {len(df1)} filmes que filtrei para você de acordo com a classificação do IMDb * :film_projector:")
+    st.write(f"### *Dá um olhada nos {len(df1)} filmes que filtrei para você de acordo com a classificação do IMDb * :film_projector:")
     bt = st.download_button("Download lista",csv,file_name='filmes_recomendados.csv')
     #st.download_button("Press to Download",convert_excel(df1))#Baixar em Excel
     st.write('')
